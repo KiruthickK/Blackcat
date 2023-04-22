28f6e6f6ed9ba50301b85d899f31ff7b41224027
KiruthickK <kiruthick101@gmail.com>
2023-04-22 12:52:18 +0530

    Small styles added

--- Php Files/HomePage.php
+++ Php Files/HomePage.php
@@ -6,24 +6,48 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home page</title>
+    <link rel="stylesheet" href="../CSS/Homepage.css">
 </head>
 
 <body onload="CheckUser()">
-    <?php
-    session_start();
-    if (isset($_SESSION["blackcatusername"])) {
-        echo ("Welcome " . $_SESSION["blackcatusername"]);
-    } else {
-        @include 'CommonMethods.php';
-        AlertError("You have to login first!");
-        RedirectPage("./login.php");
-    }
-    ?>
     <form action="HomePage.php" method="post">
-        <!-- <input type="button" name="logout" id="logout" placeholder="logout?"> -->
-        <button name="logout">Logout</button>
+        <nav>
+            <ul>
+                <li><a href="#"><button name="logout" class="custom-button">Logout</button></a></li>
+                <li><a href="#">Post</a></li>
+                <li ><a href="#">Check Posts</a></li>
+                <li class="welcome">
+                    <?php
+                    session_start();
+                    if (isset($_SESSION["blackcatusername"])) {
+                        echo ("Welcome " . $_SESSION["blackcatusername"]."!");
+                    } else {
+                        @include 'CommonMethods.php';
+                        AlertError("You have to login first!");
+                        RedirectPage("./login.php");
+                    }
+                    ?>
+                </li>
+            </ul>
+
+        </nav>
     </form>
-    <?php
+
+    <br><br>
+    <form action="Php Files/postcat.php" method="post" enctype="multipart/form-data">
+        post cat
+        <input type="text" name="username" placeholder="Enter username">
+        <input type="text" name="catname" placeholder="Enter your cat name"> Select image to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
+    <!-- <input type="button" name="logout" id="logout" placeholder="logout?"> -->
+</body>
+
+</html>
+
+<!-- backend php -->
+<?php
     @include 'CommonMethods.php';
     if (isset($_POST["logout"])) {
         // session_start();
@@ -34,13 +58,3 @@
         RedirectPage("./login.php");
     }
     ?>
-    <form action="Php Files/postcat.php" method="post" enctype="multipart/form-data">
-        post cat
-        <input type="text" name="username" placeholder="Enter username">
-        <input type="text" name="catname" placeholder="Enter your cat name"> Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Upload Image" name="submit">
-    </form>
-</body>
-
-</html>
